b79847eaf5fca5de30633ba27f4454fc3491e887
jefry22 <jeffersonpierov@gmail.com>
2024-03-11 20:10:41 -0500

    section politica - footer

--- src/scripts/reviews.ts
+++ src/scripts/reviews.ts
@@ -2,19 +2,19 @@ import type {ReviewsItem} from '../types/types';
 
 const reviews:ReviewsItem[] = [
     {
-      author_name: "Marcio Bendezú Gálvez",
+      author_name: "Alfonso bouroncle",
       rating: 4,
-      text: "Muy buen hotel, dos piscinas, bungalows acogedores, en la playa y en la parte campo se pueden apreciar cangrejos, todo tipo de aves costeñas, bufeos de vez en cuando, lagartijas... y los guacamayos del hotel son bellos. Solo la comida me pareció muy limitada pero luego de eso, ¡muy bueno! :D Excelente para relajarse y convivir con la naturaleza.",
+      text: "Comida de mar, tienen productos frescos y cocinan a tu gusto o necesidad",
     },
     {
-      author_name: "Usuario de Google",
+      author_name: "Giomar Gallegos",
       rating: 4,
-      text: "Muy buen hotel, dos piscinas, bungalows acogedores, en la playa y en la parte campo se pueden apreciar cangrejos, todo tipo de aves costeñas, bufeos de vez en cuando, lagartijas... y los guacamayos del hotel son bellos. Solo la comida me pareció muy limitada pero luego de eso, ¡muy bueno! :D Excelente para relajarse y convivir con la naturaleza.",
+      text: "Lugar muy tranquilo, agradable, sin ruidos, listo para escaparse de la rutina.",
     },
     {
-      author_name: "Usuario de Google",
+      author_name: "Julian Salazar",
       rating: 4,
-      text: "Muy buen hotel, dos piscinas, bungalows acogedores, en la playa y en la parte campo se pueden apreciar cangrejos, todo tipo de aves costeñas, bufeos de vez en cuando, lagartijas... y los guacamayos del hotel son bellos. Solo la comida me pareció muy limitada pero luego de eso, ¡muy bueno! :D Excelente para relajarse y convivir con la naturaleza.",
+      text: "Un lugar calido agradable paisaje para pasarcon familia recomendable.",
     },
   ]
 
